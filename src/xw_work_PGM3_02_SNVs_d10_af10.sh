1f5d901846b65eaff1bdca8b36317c3b8ed1e285
fzc1997 <fzc_1997@163.com>
2020-07-14 14:57:23 +0800

    fix bug:filtering file,filtering_dbSNP_vcf UCSC_RepeatMask_bed

--- src/xw_work_PGM3_02_SNVs_d10_af10.sh
+++ src/xw_work_PGM3_02_SNVs_d10_af10.sh
@@ -70,19 +70,19 @@ awk '{if($7>=10 && $8>=0.1)print $1"\t"$2-1"\t"$2"\t"$1":"$2"\t"$0}' ${work_path
 # dbSNP151 all
 {
 # gatk_d10_af10
-${dir_of_perl}/perl ${join_ID_pl} ${db_path}/backup/vcf151/01_GRCh38_b151_p7_all.txt ${work_path}/Novel_SNVs_d10_af10/${name}_12_SNVs_gatk_d10_af10.txt 1 4 |cut -f 1,4,7-9,13-18 > ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_gatk_d10_af10.txt
+${dir_of_perl}/perl ${join_ID_pl} <(bcftools view $filtering_dbSNP_vcf -H|awk 'BEGIN{OFS="\t"}{print $1":"$2,$1,$2,$3,$4}') ${work_path}/Novel_SNVs_d10_af10/${name}_12_SNVs_gatk_d10_af10.txt 1 4 |cut -f 1,4,7-9,13-18 > ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_gatk_d10_af10.txt
 cut -f 1 ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_gatk_d10_af10.txt |sort -u > ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_gatk_d10_af10.sites
 ${dir_of_perl}/perl ${select_v_ID_pl} ${work_path}/Novel_SNVs_d10_af10/${name}_12_SNVs_gatk_d10_af10.txt ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_gatk_d10_af10.sites 4 > ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_gatk_d10_af10_rmdbSNP151.txt
 }&
 {
 # lofreq_d10_af10
-${dir_of_perl}/perl ${join_ID_pl} ${db_path}/backup/vcf151/01_GRCh38_b151_p7_all.txt ${work_path}/Novel_SNVs_d10_af10/${name}_12_SNVs_lofreq_d10_af10.txt 1 4 |cut -f 1,4,7-9,13-18 > ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_lofreq_d10_af10.txt
+${dir_of_perl}/perl ${join_ID_pl} <(bcftools view $filtering_dbSNP_vcf -H|awk 'BEGIN{OFS="\t"}{print $1":"$2,$1,$2,$3,$4}') ${work_path}/Novel_SNVs_d10_af10/${name}_12_SNVs_lofreq_d10_af10.txt 1 4 |cut -f 1,4,7-9,13-18 > ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_lofreq_d10_af10.txt
 cut -f 1 ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_lofreq_d10_af10.txt |sort -u > ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_lofreq_d10_af10.sites
 ${dir_of_perl}/perl ${select_v_ID_pl} ${work_path}/Novel_SNVs_d10_af10/${name}_12_SNVs_lofreq_d10_af10.txt ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_lofreq_d10_af10.sites 4 > ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_lofreq_d10_af10_rmdbSNP151.txt
 }&
 {
 # strelka2_d10_af10
-${dir_of_perl}/perl ${join_ID_pl} ${db_path}/backup/vcf151/01_GRCh38_b151_p7_all.txt ${work_path}/Novel_SNVs_d10_af10/${name}_12_SNVs_strelka2_d10_af10.txt 1 4 |cut -f 1,4,7-9,13-18 > ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_strelka2_d10_af10.txt
+${dir_of_perl}/perl ${join_ID_pl} <(bcftools view $filtering_dbSNP_vcf -H|awk 'BEGIN{OFS="\t"}{print $1":"$2,$1,$2,$3,$4}') ${work_path}/Novel_SNVs_d10_af10/${name}_12_SNVs_strelka2_d10_af10.txt 1 4 |cut -f 1,4,7-9,13-18 > ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_strelka2_d10_af10.txt
 cut -f 1 ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_strelka2_d10_af10.txt |sort -u > ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_strelka2_d10_af10.sites
 ${dir_of_perl}/perl ${select_v_ID_pl} ${work_path}/Novel_SNVs_d10_af10/${name}_12_SNVs_strelka2_d10_af10.txt ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_strelka2_d10_af10.sites 4 > ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_strelka2_d10_af10_rmdbSNP151.txt
 }&
@@ -91,15 +91,15 @@ wait
 
 # 14. remove repeat region
 # gatk_d10_af10
-${dir_of_intersectBed}/intersectBed -a ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_gatk_d10_af10_rmdbSNP151.txt -b ${db_path}/backup/vcf151/UCSC_RepeatMask_hg38.bed -wa -wb |cut -f 4 |sort -u > ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_gatk_d10_af10_RepeatMask.sites
+${dir_of_intersectBed}/intersectBed -a ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_gatk_d10_af10_rmdbSNP151.txt -b ${UCSC_RepeatMask_bed} -wa -wb |cut -f 4 |sort -u > ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_gatk_d10_af10_RepeatMask.sites
 ${dir_of_perl}/perl ${select_v_ID_pl} ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_gatk_d10_af10_rmdbSNP151.txt ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_gatk_d10_af10_RepeatMask.sites 4 |cut -f 1-4,7-12 > ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_gatk_d10_af10_novel.txt
 cut -f 4 ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_gatk_d10_af10_novel.txt |sort -u > ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_gatk_d10_af10_novel.sites
 # lofreq_d10_af10
-${dir_of_intersectBed}/intersectBed -a ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_lofreq_d10_af10_rmdbSNP151.txt -b ${db_path}/backup/vcf151/UCSC_RepeatMask_hg38.bed -wa -wb |cut -f 4 |sort -u > ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_lofreq_d10_af10_RepeatMask.sites
+${dir_of_intersectBed}/intersectBed -a ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_lofreq_d10_af10_rmdbSNP151.txt -b ${UCSC_RepeatMask_bed} -wa -wb |cut -f 4 |sort -u > ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_lofreq_d10_af10_RepeatMask.sites
 ${dir_of_perl}/perl ${select_v_ID_pl} ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_lofreq_d10_af10_rmdbSNP151.txt ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_lofreq_d10_af10_RepeatMask.sites 4 |cut -f 1-4,7-12 > ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_lofreq_d10_af10_novel.txt
 cut -f 4 ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_lofreq_d10_af10_novel.txt |sort -u > ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_lofreq_d10_af10_novel.sites
 # strelka2_d10_af10
-${dir_of_intersectBed}/intersectBed -a ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_strelka2_d10_af10_rmdbSNP151.txt -b ${db_path}/backup/vcf151/UCSC_RepeatMask_hg38.bed -wa -wb |cut -f 4 |sort -u > ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_strelka2_d10_af10_RepeatMask.sites
+${dir_of_intersectBed}/intersectBed -a ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_strelka2_d10_af10_rmdbSNP151.txt -b ${UCSC_RepeatMask_bed} -wa -wb |cut -f 4 |sort -u > ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_strelka2_d10_af10_RepeatMask.sites
 ${dir_of_perl}/perl ${select_v_ID_pl} ${work_path}/Novel_SNVs_d10_af10/${name}_13_all_strelka2_d10_af10_rmdbSNP151.txt ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_strelka2_d10_af10_RepeatMask.sites 4 |cut -f 1-4,7-12 > ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_strelka2_d10_af10_novel.txt
 cut -f 4 ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_strelka2_d10_af10_novel.txt |sort -u > ${work_path}/Novel_SNVs_d10_af10/${name}_14_all_strelka2_d10_af10_novel.sites
 
