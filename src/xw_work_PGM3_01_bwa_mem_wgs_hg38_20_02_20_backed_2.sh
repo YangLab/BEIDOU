4557070896afc705307b645554363299cba6f0c2
Zhican FU <fuzhican@liyang-svr3.icb.ac.cn>
2020-11-30 22:28:02 +0800

    2020_11_30_22_28

--- src/xw_work_PGM3_01_bwa_mem_wgs_hg38_20_02_20_backed_2.sh
+++ src/xw_work_PGM3_01_bwa_mem_wgs_hg38_20_02_20_backed_2.sh
@@ -210,7 +210,8 @@ step7_HaplotypeCaller(){
     chrn_list=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY chrM)  
     }  
     elif [ "$genome_build_version" == "mm10" ];then  
-    chrn_list=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chrX chrY chrM)  
+    # chrn_list=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chrX chrY chrM)  
+    chrn_list=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chrX chrY )  
     fi  
     #echo ${chrn_list[@]}|awk 'BEGIN{RS=" ";ORS="\" -L \""}{print}'  
     
